fac23beecae29feb3da3505a1e8f1dc729b376dc
canseray <canseraytufan@gmail.com>
2019-04-30 18:49:36 +0200

    message activitys

---
+++ app/src/main/java/com/example/kotlin_messenger/LastMessageActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kotlin_messenger
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LastMessageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_last_message)
+    }
+}
